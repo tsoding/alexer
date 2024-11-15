9b57a4ed79c32d6642187696ba4f2c35062783c1
rexim <reximkut@gmail.com>
2024-11-16 06:58:26 +0700

    Add a basic example

---
+++ example.c
@@ -0,0 +1,31 @@
+#define ALEXER_IMPLEMENTATION
+#include "alexer.h"
+
+typedef enum {
+    PUNCT_PLUS,
+    PUNCT_MULT,
+    PUNCT_OPAREN,
+    PUNCT_CPAREN,
+} Punct_Index;
+
+const char *puncts[] = {
+    [PUNCT_PLUS] = "+",
+    [PUNCT_MULT] = "*",
+    [PUNCT_OPAREN] = "(",
+    [PUNCT_CPAREN] = ")",
+};
+
+int main()
+{
+    const char *file_path = "example/path"; // No file is specified for this example
+    const char *content = "a + (35*45)";
+    Alexer l = alexer_create(file_path, content, strlen(content));
+    l.puncts = puncts;
+    l.puncts_count = ALEXER_ARRAY_LEN(puncts);
+    Alexer_Token t = {0};
+    while (alexer_get_token(&l, &t)) {
+        l.diagf(t.loc, "INFO", "%s: %.*s", alexer_kind_name(t.kind), t.end - t.begin, t.begin);
+    }
+    if (!alexer_expect_kind(&l, t, ALEXER_END)) return 1;
+    return 0;
+}
--- nob.c
+++ nob.c
@@ -6,9 +6,13 @@ int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
-    cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-Wswitch-enum", "-ggdb", "-o", "main", "main.c");
+    //cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-Wswitch-enum", "-ggdb", "-o", "main", "main.c");
+    //if (!cmd_run_sync_and_reset(&cmd)) return 1;
+    //cmd_append(&cmd, "./main");
+    //if (!cmd_run_sync_and_reset(&cmd)) return 1;
+    cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-Wswitch-enum", "-ggdb", "-o", "example", "example.c");
     if (!cmd_run_sync_and_reset(&cmd)) return 1;
-    cmd_append(&cmd, "./main");
+    cmd_append(&cmd, "./example");
     if (!cmd_run_sync_and_reset(&cmd)) return 1;
     return 0;
 }
