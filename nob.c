ecd1932b3908be85871329b71d5dda9a82a8ce3f
rexim <reximkut@gmail.com>
2024-11-16 07:01:03 +0700

    Remove grammar.bnf related stuff

--- nob.c
+++ nob.c
@@ -6,10 +6,6 @@ int main(int argc, char **argv)
 {
     NOB_GO_REBUILD_URSELF(argc, argv);
     Cmd cmd = {0};
-    //cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-Wswitch-enum", "-ggdb", "-o", "main", "main.c");
-    //if (!cmd_run_sync_and_reset(&cmd)) return 1;
-    //cmd_append(&cmd, "./main");
-    //if (!cmd_run_sync_and_reset(&cmd)) return 1;
     cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-Wswitch-enum", "-ggdb", "-o", "example", "example.c");
     if (!cmd_run_sync_and_reset(&cmd)) return 1;
     cmd_append(&cmd, "./example");
